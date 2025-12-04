9a71a44a38559a90693c18f229098143215cc5d9
felixwebman <felixosman168@gmail.com>
2025-12-04 12:25:27 +0530

    Update footer.php

--- felixaffiliro-theme/footer.php
+++ felixaffiliro-theme/footer.php
@@ -1,1 +1,18 @@
+<footer>
+  <div class="footer-links">
+    <a href="<?php echo home_url(); ?>">Home</a>
+    <a href="#">Products</a>
+    <a href="<?php echo home_url('/blog'); ?>">Blog</a>
+    <a href="#">Contact</a>
+  </div>
 
+  <div>
+    &copy; <?php echo date('Y'); ?> Felixaffiliro.com |
+    <a href="#">Privacy Policy</a> |
+    <a href="#">Terms & Conditions</a>
+  </div>
+</footer>
+
+<?php wp_footer(); ?>
+</body>
+</html>
