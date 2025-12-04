fb6c3654b41af702546e544488ef57e8991790e4
felixwebman <felixosman168@gmail.com>
2025-12-04 12:26:01 +0530

    Update functions.php

--- felixaffiliro-theme/functions.php
+++ felixaffiliro-theme/functions.php
@@ -1,1 +1,8 @@
+<?php
+
+function felix_theme_files() {
+    wp_enqueue_style('felix-style', get_stylesheet_uri());
+    wp_enqueue_script('felix-js', get_template_directory_uri() . '/script.js', array(), false, true);
+}
+add_action('wp_enqueue_scripts', 'felix_theme_files');
 
